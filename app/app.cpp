60017f85e6cc424289330c84db8e38461a12e679
Tessa Power <hello@tessapower.xyz>
2024-08-02 14:27:03 -0500

    Add basic project structure

---
+++ app/app.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/opencv.hpp>
+
+int main() {
+
+  // TODO: Create a window
+  // TODO: select load video from disk
+  // TODO: detect(?) and set frame rate
+  // TODO: play the original and stabilized video for side-by-side comparison
+  // TODO: save video to disk
+
+  return EXIT_SUCCESS;
+}
