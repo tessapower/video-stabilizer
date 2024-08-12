49b4f9856e092dd325da2b0f694c907f2635129c
Tessa Power <hello@tessapower.xyz>
2024-08-12 09:15:03 -0500

    Add static variables for window dimensions

--- app/app.cpp
+++ app/app.cpp
@@ -1,3 +1,35 @@
+/// PROJECT: Video Stabilizer
+/// DESCRIPTION: Stabilizes and crops a video.
+///
+/// AUTHOR: Tessa Power
+/// DATE: August 1st, 2024
+///
+/// LICENSE: MIT License
+///
+/// Copyright(c) 2024 Tessa Power
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a
+/// copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without
+/// limitation the rights to use, copy, modify, merge, publish, distribute,
+/// sublicense, and/or sell copies of the Software, and to permit persons to
+/// whom the Software is furnished to do so, subject to the following
+/// conditions:
+///
+///   The above copyright notice and this permission notice shall be included
+///   in all copies or substantial portions of the Software.
+///
+///   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+///   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+///   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+///   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+///   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+///   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+///   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+///
+/// NOTES: This project expects that OpenCV is installed locally on your system
+///
+
 #include "app/app.h"
 
 #include <GLFW/glfw3.h>
@@ -24,8 +56,9 @@ auto main() -> int {
   // Do not support resizing the window
   glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-  const auto window =
-      glfwCreateWindow(500, 500, "Video Stabilizer", nullptr, nullptr);
+  const auto window = glfwCreateWindow(app::window_width, app::window_height,
+                                       "Video Stabilizer", nullptr, nullptr);
+
   if (!window) {
     std::cerr << "Error: Could not create GLFW window" << '\n';
     abort();
