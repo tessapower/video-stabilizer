e5fe26546c34b4a3624020eb7f2478aea55bf371
Tessa Power <hello@tessapower.xyz>
2024-08-23 16:47:00 -0500

    Support selecting multiple images to import as video

--- include/utils.h
+++ include/utils.h
@@ -44,6 +44,56 @@ inline auto get_video_path(GLFWwindow* window, std::string& path) -> bool {
 
   return false;
 }
+
+inline auto get_frame_paths(GLFWwindow* window, std::vector<std::string>& paths) -> bool {
+  const nfdpathset_t* out_paths;
+  constexpr nfdu8filteritem_t filters[1] = {
+      {"images", "jpg,jpeg,png"}};
+  nfdopendialogu8args_t args = {nullptr, 0, nullptr};
+  NFD_GetNativeWindowFromGLFWWindow(window, &args.parentWindow);
+  args.filterList = filters;
+  args.filterCount = 1;
+  // Pass in the address of the pointer to the path set and the args
+  nfdresult_t result = NFD_OpenDialogMultipleU8_With(&out_paths, &args);
+
+  switch (result) {
+    case NFD_OKAY: {
+      std::cout << "Success!\n";
+
+      // Iterate over the paths in the path set
+      // Declare enumerator (not a pointer)
+      nfdpathsetenum_t enumerator;
+      NFD_PathSet_GetEnum(out_paths, &enumerator);
+      nfdchar_t* path;
+      unsigned i = 0;
+      while (NFD_PathSet_EnumNext(&enumerator, &path) && path) {
+        paths.emplace_back(path);
+        std::cout << "Path " << i++ << ": " << path << "\n";
+
+        // Free the path set path with NFD_PathSet_FreePath (not NFD_FreePath!)
+        NFD_PathSet_FreePath(path);
+      }
+
+      // Free the path set enumerator memory (before freeing the path set)
+      NFD_PathSet_FreeEnum(&enumerator);
+
+      // Free the path set memory (since NFD_OKAY is returned)
+      NFD_PathSet_Free(out_paths);
+
+      return true;
+    }
+    case NFD_CANCEL: {
+      std::cout << "User pressed cancel.\n";
+      return false;
+    }
+    case NFD_ERROR: {
+      std::cerr << "Error: " << NFD_GetError() << '\n';
+      return false;
+    }
+  }
+
+  return false;
+}
 }  // namespace utils
 
 #endif  // UTILS_H
