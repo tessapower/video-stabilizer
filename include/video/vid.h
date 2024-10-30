f04fbaa1bcf3fc0cf3618305e976eb8159d6c2b0
Tessa Power <hello@tessapower.xyz>
2024-10-30 11:25:50 -0500

    Refactor video class

--- include/video/vid.h
+++ include/video/vid.h
@@ -2,23 +2,19 @@
 #define VIDEO_H
 
 #include <filesystem>
+#include <opencv2/videoio.hpp>
 #include <opencv2/core/mat.hpp>
 
 namespace vid {
 class video {
  public:
   video();
-  video& operator=(const video& other) noexcept;  // Copy-Assignment Operator
   explicit video(std::filesystem::path const& video_file_path);
+  video(video const& other);      // Copy Constructor
+  video(video&& other) noexcept;  // Move Constructor
   ~video() = default;
 
-  // Delete unused constructors and assignment operators
-  video(video const& other) = delete; // Copy Constructor
-  video(video const&& other) = delete; // Move Constructor
-  video& operator=(const video&& other) = delete; // Move-Assignment Operator
-
-  auto load_video_from_file(std::string const& video_file_path) noexcept
-      -> void;
+  video& operator=(video const& other) = default;  // Copy-Assignment Operator
 
   auto load_video_from_file(
       std::filesystem::path const& video_file_path) noexcept -> void;
@@ -30,7 +26,7 @@ class video {
       -> bool;
 
   [[nodiscard]] auto empty() const noexcept -> bool {
-    return original_frames_.empty();
+    return frame_count_ == 0;
   }
 
   [[nodiscard]] auto fps() const noexcept -> int { return fps_; }
@@ -39,19 +35,36 @@ class video {
 
   [[nodiscard]] auto bitrate() const noexcept -> double { return bitrate_; }
 
+  [[nodiscard]] auto frame_count() const noexcept -> int {
+    return frame_count_;
+  }
+
+  [[nodiscard]] auto frames() const noexcept -> std::vector<cv::Mat> {
+    return frames_;
+  }
+
+  auto frames(std::vector<cv::Mat> const& new_frames) noexcept -> void {
+    frames_ = new_frames;
+    frame_count_ = static_cast<int>(frames_.size());
+    size_ = frames_[0].size();
+  }
+
+  [[nodiscard]] auto clone() const noexcept -> video;
+
  private:
-  std::vector<cv::Mat> original_frames_{};
   std::string file_name_;
+  std::vector<cv::Mat> frames_{};
   double bitrate_ = 0;
   int fourcc_ = 0;
   int fps_ = 0;
   int frame_count_ = 0;
   cv::Size size_;
 
+  auto process_video(std::filesystem::path const& video_file_path) noexcept -> void;
+
   auto load_frames(std::vector<std::string> const& frames_file_paths) noexcept -> void;
 
-  static auto padded_string(const int n, const int frame_count) noexcept
-      -> std::string;
+  static auto padded_string(int n, int frame_count) noexcept -> std::string;
 };
 }  // namespace vid
 
--- src/video/vid.cpp
+++ src/video/vid.cpp
@@ -2,6 +2,7 @@
 
 #include <opencv2/core/core_c.h>
 
+#include <ranges>
 #include <filesystem>
 #include <iostream>
 #include <opencv2/core.hpp>
@@ -9,26 +10,50 @@
 #include <opencv2/videoio.hpp>
 
 #include "logger/logger.h"
-#include "video/stabilizer.h"
 
 namespace vid {
 video::video() {
-  original_frames_ = std::vector<cv::Mat>{};
-  stabilizer_ = vid::stabilizer{};
-  fps_ = 0;
+  frames_ = std::vector<cv::Mat>{};
+
+  bitrate_ = 0.0;
+  frame_count_ = static_cast<int>(frames_.size());
   fourcc_ = 0;
+  fps_ = 0;
+  size_ = {0, 0};
 }
 
-video& video::operator=(video const& other) noexcept {
+video::video(video const& other) {
   if (this != &other) {
-    original_frames_ = other.original_frames_;
-    stabilizer_ = other.stabilizer_;
-    fps_ = other.fps_;
+    std::ranges::copy(other.frames_,
+                      std::back_inserter(frames_));
+    file_name_ = other.file_name_;
+
+    bitrate_ = other.bitrate_;
     fourcc_ = other.fourcc_;
+    fps_ = other.fps_;
+    frame_count_ = static_cast<int>(frames_.size());
     size_ = other.size_;
   }
+}
 
-  return *this;
+video::video(video&& other) noexcept {
+  if (this != &other) {
+    std::ranges::move(other.frames_,
+                      std::back_inserter(frames_));
+
+    file_name_ = other.file_name_;
+    other.file_name_ = "";
+    bitrate_ = other.bitrate_;
+    other.bitrate_ = 0;
+    fourcc_ = other.fourcc_;
+    other.fourcc_ = 0;
+    fps_ = other.fps_;
+    other.fps_ = 0;
+    frame_count_ = static_cast<int>(frames_.size());
+    other.frame_count_ = 0;
+    size_ = other.size_;
+    other.size_ = {0, 0};
+  }
 }
 
 video::video(std::filesystem::path const& video_file_path) {
@@ -40,7 +65,7 @@ video::video(std::filesystem::path const& video_file_path) {
 auto video::load_frames(
     std::vector<std::string> const& frames_file_paths) noexcept -> void {
   // Clear out old data
-  original_frames_.clear();
+  frames_.clear();
 
   logger::instance()->add_dynamic_log("load-frames", [&]() -> std::string {
     return std::string("Loading frames") + utils::loading_dots() + "\n";
@@ -54,7 +79,7 @@ auto video::load_frames(
       continue;
     }
 
-    original_frames_.push_back(frame);
+    frames_.push_back(frame);
   }
 
   logger::instance()->remove_dynamic_log("load-frames");
@@ -75,28 +100,51 @@ auto video::padded_string(const int n, const int frame_count) noexcept
 auto video::load_video_from_file(
     std::filesystem::path const& video_file_path) noexcept -> void {
   // Clear out old data
-  if (!original_frames_.empty()) {
-    original_frames_.clear();
-    fps_ = 0;
+  if (!frames_.empty()) {
+    frames_.clear();
     file_name_ = "";
+    bitrate_ = 0.0;
     fourcc_ = 0;
+    fps_ = 0;
+    frame_count_ = 0;
+    size_ = {0, 0};
   }
 
+  process_video(video_file_path);
+
+  // Read all the frames in the tmp folder into frames vector
+  std::vector<cv::String> image_paths;
+  cv::glob(".//tmp//", image_paths);
+
+  load_frames(image_paths);
+
+  // Delete all tmp files
+  std::filesystem::remove_all(".//tmp//");
+}
+
+auto video::process_video(std::filesystem::path const& video_file_path) noexcept
+    -> void {
   // Create a VideoCapture Object
   auto video_capture = cv::VideoCapture(video_file_path.string());
 
-  if (!video.isOpened()) {
+  if (!video_capture.isOpened()) {
     std::cerr << "Error: Could not open video file\n";
 
     return;
   }
 
-  bitrate_ = video.get(cv::CAP_PROP_BITRATE);
-  fourcc_ = static_cast<int>(video.get(cv::CAP_PROP_FOURCC));
-  fps_ = static_cast<int>(video.get(cv::CAP_PROP_FPS));
-  frame_count_ = static_cast<int>(video.get(cv::CAP_PROP_FRAME_COUNT));
-  size_ = cv::Size(static_cast<int>(video.get(cv::CAP_PROP_FRAME_WIDTH)),
-                   static_cast<int>(video.get(cv::CAP_PROP_FRAME_HEIGHT)));
+  // Add dynamic log to track progress of video load
+  logger::instance()->add_dynamic_log("progress", [&]() -> std::string {
+    return std::string("Processing video") + utils::loading_dots() + "\n";
+  });
+
+  bitrate_ = video_capture.get(cv::CAP_PROP_BITRATE);
+  fourcc_ = static_cast<int>(video_capture.get(cv::CAP_PROP_FOURCC));
+  fps_ = static_cast<int>(video_capture.get(cv::CAP_PROP_FPS));
+  frame_count_ = static_cast<int>(video_capture.get(cv::CAP_PROP_FRAME_COUNT));
+  size_ =
+      cv::Size(static_cast<int>(video_capture.get(cv::CAP_PROP_FRAME_WIDTH)),
+               static_cast<int>(video_capture.get(cv::CAP_PROP_FRAME_HEIGHT)));
 
   // TODO: convert to debug log
   std::cout << "Opened video file: " << video_file_path.string() << "\n";
@@ -111,43 +159,27 @@ auto video::load_video_from_file(
   // Extract the frames from the video into the tmp directory
   cv::Mat frame;
 
-  // Add dynamic log to track progress of video load
-  logger::instance()->add_dynamic_log("progress", [&]() -> std::string {
-    return std::string("Processing video") + utils::loading_dots() + "\n";
-  });
-
   auto i = 0;
-  while (video.read(frame)) {
-    const auto file_name = std::string{
-        ".//tmp//frame_" + padded_string(i++, frame_count_) + ".png"};
-    cv::imwrite(file_name, frame);
+  while (video_capture.read(frame)) {
+    const auto tmp = std::string{".//tmp//frame_" +
+                                 padded_string(i++, frame_count_) + ".png"};
+    cv::imwrite(tmp, frame);
   }
 
   logger::instance()->remove_dynamic_log("progress");
-
-  // Read all the frames in the tmp folder into frames vector
-  std::vector<cv::String> image_paths;
-  cv::glob(".//tmp//", image_paths);
-
-  load_frames(image_paths);
-
-  // Delete all tmp files
-  std::filesystem::remove_all(".//tmp//");
-}
-
 }
 
 auto video::export_to_file(std::string const& save_dir) const noexcept -> bool {
-  const auto stabilized_frames = stabilizer_.stabilized_frames();
-  if (stabilized_frames.empty()) {
+  if (frames_.empty()) {
     // TODO: convert to debug log
-    std::cerr << "Error: No stabilized frames to export\n";
+    std::cerr << "Error: No frames to export\n";
+
     return false;
   }
 
   // TODO: support user setting name of file
-  const auto save_location = std::string{save_dir + "/stabilized_video_0.avi"};
-  const auto dimensions = stabilized_frames[0].size();
+  const auto save_location = std::string{save_dir + "/video_0.avi"};
+  const auto dimensions = frames_[0].size();
 
   // TODO: convert to debug log
   std::cout << "FPS: " << fps_ << "\n";
@@ -169,13 +201,29 @@ auto video::export_to_file(std::string const& save_dir) const noexcept -> bool {
 
   // TODO: convert to debug log
   std::cout << "Using " << writer.getBackendName() << " to write new file.\n";
-  std::cout << stabilized_frames.size() << " frames to write\n";
+  std::cout << frame_count_ << " frames to write\n";
 
-  for (auto const& frame : stabilized_frames) {
+  for (auto const& frame : frames_) {
     // Encode the frame into the video file stream
     writer.write(frame);
   }
 
   return true;
+}
+
+auto video::clone() const noexcept -> video {
+  vid::video cloned{};
+
+  std::ranges::copy(frames_,
+                    std::back_inserter(cloned.frames_));
+
+  cloned.file_name_ = file_name_;
+  cloned.bitrate_ = bitrate();
+  cloned.fourcc_ = fourcc();
+  cloned.fps_ = fps();
+  cloned.frame_count_ = frame_count();
+  cloned.size_ = size_;
+
+  return cloned;
 }
 }  // namespace vid
